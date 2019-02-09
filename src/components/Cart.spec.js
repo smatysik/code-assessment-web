fc13086ecd126b0c87bd3d9259954fca35d3c959
Steve Matysik <smatysik@gmail.com>
2019-02-08 21:36:12 -0500

    fixes cart tests by using json stringify to avoid anonymous function matching issue

--- src/components/Cart.spec.js
+++ src/components/Cart.spec.js
@@ -9,7 +9,8 @@ Enzyme.configure({ adapter: new Adapter() })
 
 const setup = (total, products = []) => {
   const actions = {
-    onCheckoutClicked: jest.fn()
+    onCheckoutClicked: jest.fn(),
+    modifyQuantity: jest.fn()
   }
 
   const component = Enzyme.shallow(
@@ -19,28 +20,23 @@ const setup = (total, products = []) => {
   return {
     component: component,
     actions: actions,
-    button: component.find(Button),
+    button: component.find(Button).at(3),
     products: component.find(Product),
     em: component.find('.no-items span').at(1),
-    p: component.find('p')
+    p: component.find('.totals span').at(1)
   }
 }
 
 describe('Cart component', () => {
-  // it('should display total', () => {
-  //   const { p } = setup('76')
-  //   expect(p.text()).toMatch(/^Total: \$76/)
-  // })
-
   it('should display add some products message', () => {
     const { em } = setup()
     expect(em.text()).toMatch(/^Please add some products to your cart/)
   })
 
-  // it('should disable button', () => {
-  //   const { button } = setup()
-  //   expect(button.prop('disabled')).toEqual('disabled')
-  // })
+  it('should not render checkout button', () => {
+    const { button } = setup()
+    expect(button.exists()).toBeFalsy()
+  })
 
   describe('when given product', () => {
     const product = [
@@ -48,22 +44,41 @@ describe('Cart component', () => {
         id: 1,
         title: 'Product 1',
         price: 9.99,
-        quantity: 1
+        quantity: 1,
+        image: 'product-image.jpg'
       }
     ]
 
     it('should render products', () => {
-      const { products } = setup('9.99', product)
+      const { products, actions } = setup('9.99', product)
       const props = {
         title: product[0].title,
         price: product[0].price,
-        quantity: product[0].quantity
+        quantity: product[0].quantity,
+        image: 'product-image.jpg',
+        children: (
+          <Button
+            onClick={() => {
+              actions.modifyQuantity(1, 0)
+            }}
+            styleName="text-link remove-btn"
+          >
+            Remove
+          </Button>
+        )
       }
 
-      expect(products.at(0).props()).toEqual(props)
+      expect(JSON.parse(JSON.stringify(products.at(0).props()))).toEqual(
+        JSON.parse(JSON.stringify(props))
+      )
     })
 
-    it('should not disable button', () => {
+    it('should display total', () => {
+      const { p } = setup('76', product)
+      expect(p.text()).toMatch(/^\$76/)
+    })
+
+    it('should render active button', () => {
       const { button } = setup('9.99', product)
       expect(button.prop('disabled')).toEqual('')
     })
