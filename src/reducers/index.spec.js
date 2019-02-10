987ba634f3eb3ae1dc0c6d355e9e49b2d66b4d24
Steve Matysik <smatysik@gmail.com>
2019-02-10 15:49:23 -0500

    adds tests for subtotal and tax amounts

--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -1,7 +1,7 @@
 import { getSubtotal, getTax, getTotal, getCartProducts } from './index'
 
 describe('selectors', () => {
-  describe('getTotal', () => {
+  describe('getSubtotalTotal', () => {
     it('should return price total', () => {
       const state = {
         cart: {
@@ -30,6 +30,70 @@ describe('selectors', () => {
         }
       }
       expect(getSubtotal(state)).toBe('27.93')
+    })
+  })
+
+  describe('getTax', () => {
+    it('should return price total', () => {
+      const state = {
+        cart: {
+          addedIds: [1, 2, 3],
+          quantityById: {
+            1: 4,
+            2: 2,
+            3: 1
+          }
+        },
+        products: {
+          byId: {
+            1: {
+              id: 1,
+              price: 1.99
+            },
+            2: {
+              id: 1,
+              price: 4.99
+            },
+            3: {
+              id: 1,
+              price: 9.99
+            }
+          }
+        }
+      }
+      expect(getTax(state)).toBe('2.48')
+    })
+  })
+
+  describe('getTotal', () => {
+    it('should return price total', () => {
+      const state = {
+        cart: {
+          addedIds: [1, 2, 3],
+          quantityById: {
+            1: 4,
+            2: 2,
+            3: 1
+          }
+        },
+        products: {
+          byId: {
+            1: {
+              id: 1,
+              price: 1.99
+            },
+            2: {
+              id: 1,
+              price: 4.99
+            },
+            3: {
+              id: 1,
+              price: 9.99
+            }
+          }
+        }
+      }
+      expect(getTotal(state)).toBe('30.41')
     })
   })
 
