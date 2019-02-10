325f6ec1668cca939ebca6e981489f466510f5cd
Steve Matysik <smatysik@gmail.com>
2019-02-10 13:50:22 -0500

    adds package to fetch from url, updates getProducts function to use callback

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,13 +1,19 @@
 /**
  * Mocking client-server processing
  */
-import _products from './products.json'
+import fetch from 'cross-fetch'
 
 const TIMEOUT = 100
 
 export default {
-  getProducts: (cb, timeout) =>
-    setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: (cb, timeout) => {
+    fetch('http://tech.work.co/shopping-cart/products.json')
+      .then(
+        response => response.json(),
+        error => console.log('An error occurred.', error)
+      )
+      .then(json => cb(json))
+  },
   buyProducts: (payload, cb, timeout) =>
     setTimeout(() => cb(), timeout || TIMEOUT)
 }
